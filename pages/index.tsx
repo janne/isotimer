57e14094fddece55dce435de3155cebabb16f9cb
Jan Andersson <janandersson@spotify.com>
2021-12-08 16:25:29 +0100

    Tweak styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,19 +18,19 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>IsoTimer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         {exercise && (
           <>
-            <h1 className="text-6xl font-bold">{exercise.name}</h1>
-            <h2 className="text-4xl font-bold">{exercise.timeLeft}s</h2>
+            <h1 className="text-6xl font-bold m-4">{exercise.name}</h1>
+            <h2 className="text-4xl font-bold m-4">{exercise.timeLeft}s</h2>
           </>
         )}
         <button
-          className="border-2 p-2 rounded"
+          className="border-2 p-2 rounded m-4"
           onClick={() => {
             setTimeStarted(Date.now());
             setExercise(findScheduled(0));
