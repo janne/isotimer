a53a7142f621347ed039ffa3c1c65d0970ff3c4f
Jan Andersson <janandersson@spotify.com>
2021-12-08 17:28:18 +0100

    Enable dark mode and reponsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   }, [timeStarted]);
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 dark:bg-gray-800">
       <Head>
         <title>IsoTimer</title>
         <link rel="icon" href="/favicon.ico" />
@@ -25,12 +25,12 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         {exercise && (
           <>
-            <h1 className="text-6xl font-bold m-4">{exercise.name}</h1>
-            <h2 className="text-4xl font-bold m-4">{exercise.timeLeft}s</h2>
+            <h1 className="text-xl sm:text-6xl font-bold m-4 dark:text-gray-300">{exercise.name}</h1>
+            <h2 className="text-lg sm:text-4xl font-bold m-4 dark:text-gray-300">{exercise.timeLeft}s</h2>
           </>
         )}
         <button
-          className="border-2 p-2 rounded m-4 bg-gray-50"
+          className="border-2 p-2 rounded m-4 bg-gray-50 dark:border-gray-500 dark:bg-gray-900 dark:text-gray-300"
           onClick={() => {
             setTimeStarted(Date.now());
             setExercise(findScheduled(0));
